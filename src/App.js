5681bbf022e7d0313e5b23802dacbb8ae8f83b57
Julien Ospital <ospital.julien@gmail.com>
2023-03-31 10:38:20 +0200

    v2

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const API_KEY = "56811cc41f3f4adf84482924233103";
+  const [weatherData, setWeatherData] = useState(null);
+  const [city, setCity] = useState("");
+
+  const getWeatherData = async (e) => {
+    e.preventDefault();
+    const url = `http://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${city}&aqi=no
+    `;
+    const response = await fetch(url);
+    const data = await response.json();
+    setWeatherData(data);
+    console.log(weatherData);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={getWeatherData}>
+        <input
+          type="text"
+          value={city}
+          onChange={(e) => setCity(e.target.value)}
+        />
+        <button type="submit">Get Weather</button>
+      </form>
+      {weatherData && (
+        <div>
+          <h2>{weatherData.location.name}</h2>
+        </div>
+      )}
     </div>
   );
 }
