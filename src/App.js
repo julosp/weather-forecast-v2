f472758b63cf99ed33c4a772e33b46ba21673979
Julien Ospital <ospital.julien@gmail.com>
2023-04-25 10:31:32 +0200

    back at it - starting css

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
-import "./App.css";
+import "./sass/style.scss";
+import logo from "./assets/img/logo_light.png";
 
 function App() {
   const API_KEY = "56811cc41f3f4adf84482924233103";
@@ -43,53 +44,61 @@ function App() {
 
   return (
     <div className="App">
-      <form onSubmit={getWeatherData}>
-        <input
-          type="text"
-          value={city}
-          onChange={(e) => setCity(e.target.value)}
-        />
-        <button type="submit">Get Weather</button>
-      </form>
-      {loading && <p>Loading...</p>}
-      {!loading && (
-        <div>
+      <div className="global_wrap">
+        <header className="header">
+          <img src={logo} alt="logo" className="header_logo" />
+          <h1 className="header_title">Petit Nuage</h1>
+        </header>
+        <div className="">
+          <form onSubmit={getWeatherData}>
+            <input
+              type="text"
+              value={city}
+              onChange={(e) => setCity(e.target.value)}
+            />
+            <button type="submit">Get Weather</button>
+          </form>
+        </div>
+        {loading && <p>Loading...</p>}
+        {!loading && (
           <div>
-            {location.name}, {location.region}{" "}
-          </div>
+            <div>
+              {location.name}, {location.region}{" "}
+            </div>
 
-          {weatherData && (
-            <>
-              <div>
-                <p> {weatherData.temp_c}°C,</p>
-                <p>{weatherData.condition.text}</p>
-                <img src={weatherData.condition.icon} alt="icon" />
-              </div>
+            {weatherData && (
+              <>
+                <div>
+                  <p> {weatherData.temp_c}°C,</p>
+                  <p>{weatherData.condition.text}</p>
+                  <img src={weatherData.condition.icon} alt="icon" />
+                </div>
 
-              <div>
-                {tempHours.map((temp) => (
-                  <div key={temp.time}>
-                    <p>{temp.time.split(" ").pop()}h</p>
-                    <p>{temp.temp_c}°C</p>
-                  </div>
-                ))}
-              </div>
-              <div>
-                <div key={forecast.date}>
-                  {forecast.map((forecast) => (
-                    <>
-                      <p>{forecast.date.split("-").pop()}</p>
-                      <img src={forecast.day.condition.icon} alt="icon" />
-                      <p>{forecast.day.mintemp_c}°C</p>
-                      <p>{forecast.day.maxtemp_c}°C</p>
-                    </>
+                <div>
+                  {tempHours.map((temp) => (
+                    <div key={temp.time}>
+                      <p>{temp.time.split(" ").pop()}h</p>
+                      <p>{temp.temp_c}°C</p>
+                    </div>
                   ))}
                 </div>
-              </div>
-            </>
-          )}
-        </div>
-      )}
+                <div>
+                  <div key={forecast.date}>
+                    {forecast.map((forecast) => (
+                      <>
+                        <p>{forecast.date.split("-").pop()}</p>
+                        <img src={forecast.day.condition.icon} alt="icon" />
+                        <p>{forecast.day.mintemp_c}°C</p>
+                        <p>{forecast.day.maxtemp_c}°C</p>
+                      </>
+                    ))}
+                  </div>
+                </div>
+              </>
+            )}
+          </div>
+        )}
+      </div>
     </div>
   );
 }
