bebe8bb79d634b6c7aa73f6231cac39abe98d8e5
Julien Ospital <ospital.julien@gmail.com>
2023-04-26 10:42:41 +0200

    more css, working on slider

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import { FiSearch } from "react-icons/fi";
 import "./sass/style.scss";
 import logo from "./assets/img/logo_light.png";
 
@@ -16,6 +17,7 @@ function App() {
     e.preventDefault();
     isLoading(true);
     const url = `http://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${city}&days=8
+
     `;
     try {
       const response = await fetch(url);
@@ -49,53 +51,74 @@ function App() {
           <img src={logo} alt="logo" className="header_logo" />
           <h1 className="header_title">Petit Nuage</h1>
         </header>
-        <div className="">
-          <form onSubmit={getWeatherData}>
-            <input
-              type="text"
-              value={city}
-              onChange={(e) => setCity(e.target.value)}
-            />
-            <button type="submit">Get Weather</button>
-          </form>
+        <div className="input-wrap">
+          <section className="input">
+            <form onSubmit={getWeatherData} className="input_form">
+              <input
+                type="text"
+                value={city}
+                onChange={(e) => setCity(e.target.value)}
+                className="input_form_text"
+                placeholder="Enter your city..."
+              />
+              <button type="submit" className="input_form_button">
+                <FiSearch />
+              </button>
+            </form>
+          </section>
         </div>
         {loading && <p>Loading...</p>}
         {!loading && (
-          <div>
-            <div>
-              {location.name}, {location.region}{" "}
-            </div>
+          <div className="weather-wrap">
+            <section className="weather">
+              <div className="weather_city">
+                <span className="weather_city_city">{location.name}</span>,
+                <br />{" "}
+                <span className="weather_city_region">
+                  {location.region}
+                </span>{" "}
+              </div>
 
-            {weatherData && (
-              <>
-                <div>
-                  <p> {weatherData.temp_c}°C,</p>
-                  <p>{weatherData.condition.text}</p>
-                  <img src={weatherData.condition.icon} alt="icon" />
-                </div>
-
-                <div>
-                  {tempHours.map((temp) => (
-                    <div key={temp.time}>
-                      <p>{temp.time.split(" ").pop()}h</p>
-                      <p>{temp.temp_c}°C</p>
-                    </div>
-                  ))}
-                </div>
-                <div>
-                  <div key={forecast.date}>
-                    {forecast.map((forecast) => (
-                      <>
-                        <p>{forecast.date.split("-").pop()}</p>
-                        <img src={forecast.day.condition.icon} alt="icon" />
-                        <p>{forecast.day.mintemp_c}°C</p>
-                        <p>{forecast.day.maxtemp_c}°C</p>
-                      </>
-                    ))}
+              {weatherData && (
+                <>
+                  <div className="weather_actual">
+                    <p className="weather_actual_temp">
+                      {" "}
+                      {weatherData.temp_c}°C
+                    </p>
+                    <p className="weather_actual_condition">
+                      {weatherData.condition.text}
+                    </p>
+                    <img src={weatherData.condition.icon} alt="icon" />
                   </div>
-                </div>
-              </>
-            )}
+
+                  <div className="weather_prev">
+                    <div className="slider">
+                      {tempHours.map((temp) => (
+                        <div key={temp.time}>
+                          <div className="weather_prev_selected">
+                            <p>{temp.time.split(" ").pop()}h</p>
+                            <p>{temp.temp_c}°C</p>
+                          </div>
+                        </div>
+                      ))}
+                    </div>
+                  </div>
+                  <div>
+                    <div key={forecast.date}>
+                      {forecast.map((forecast) => (
+                        <>
+                          <p>{forecast.date.split("-").pop()}</p>
+                          <img src={forecast.day.condition.icon} alt="icon" />
+                          <p>{forecast.day.mintemp_c}°C</p>
+                          <p>{forecast.day.maxtemp_c}°C</p>
+                        </>
+                      ))}
+                    </div>
+                  </div>
+                </>
+              )}
+            </section>
           </div>
         )}
       </div>
